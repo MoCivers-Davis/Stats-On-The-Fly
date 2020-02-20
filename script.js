4824af86c21c0b147ee1e401a1f30263865c5af7
Antonio Alvarado <ant.36alv@gmail.com>
2020-02-20 11:14:27 -0500

    Stats work, just need to pick out which ones to dispaly and tie it up with index.html. Stats given are the the season average for the current year. If the player did not play this year then it gives the season average for whatever it returns.

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-//works
-document.write("hello")
-
+// var queryURL = "https://cors-anywhere.herokuapp.com/https://www.balldontlie.io/api/v1/players/237"
+var jawn = "https://www.balldontlie.io/api/v1/stats"
+var searchTerm = "embiid"
+var playerID;
+const corsHelp = "https://cors-anywhere.herokuapp.com/"
+var queryURL = corsHelp + "https://www.balldontlie.io/api/v1/players/?search=" + searchTerm;
 $.ajax({
     //url : "https://stats.nba.com/stats/playercareerstats/?"//scoreboard/?GameDate=01/28/2020&LeagueID=00",//&LeagueID=00&DayOffset=0",//"https://stats.nba.com/stats/
     //zplayercareerstats",
-    url : "https://stats.nba.com/stats/scoreboard/?GameDate=01/28/2020&LeagueID=00&DayOffset=0",
-    method : "GET"
-}).then( (response) => {
-  
-    console.log("stuff")
-  
+    url: queryURL,//"stats.nba.com/stats/scoreboard/?GameDate=02/14/2015&LeagueID=00&DayOffset=0",
+    method: "GET"
+}).then((response) => {
+
     console.log(response)
+    //TODO change back playerid back to let and restrucutre function order
+    playerID = response.data[0].id
+    $.ajax({//last 25 gamees or so
+        url: corsHelp + "https://www.balldontlie.io/api/v1/stats/?player_ids[]=" + playerID,
+        method: "GET"
+    }).then((games) => {
+        console.log(games)
+        console.log(games.data[0].game.date)
+        let date = games.data[0].game.date
+        let year = date.substring(0, 4)
+
+        $.ajax({
+            url: corsHelp + "https://www.balldontlie.io/api/v1/season_averages/"/*?season=" */ + "?player_ids[]=" + playerID,//year + "&player_ids[]=" + playerID,// + "&player_ids=" + playerID,
+            method: "GET"
+        }).then((playerStats) => {
+            // console.log(year)
+            //if the player did not record stats in the current year then we will go for whatever year the stats previously were recorded
+            if (playerStats.data.length == 0) {
+                $.ajax({
+                    url: corsHelp + "https://www.balldontlie.io/api/v1/season_averages/?season=" + year + "&player_ids[]=" + playerID,
+                    method: "GET"
+                }).then((again) => {
+                    console.log(year)
+                    console.log(again)
+                })
+            } else {
+                console.log(playerStats)
+            }
+        })
+    })
 })
